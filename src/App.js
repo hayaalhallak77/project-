c514ecea3cd0d7aef6ef7a086c65ae0ae8c5fbcc
hayaalhallak77 <142922147+hayaalhallak77@users.noreply.github.com>
2023-12-08 01:50:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarHA from './Componets/Navbar/NavbarHA';
+import Home from './Page/Home/Home';
+import Services from './Page/Services/Services';
+import Complaints from './Page/Complaints/Complaints';
+import Electricity from './Page/Electricity/Electricity';
+import News from './Page/News/News';
+import Martyrs from './Page/Martyrs/Martyrs';
+import Footer from './Componets/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+    <NavbarHA/>  
+       <Home/>    
+          <Services/>   
+         <Complaints/> 
+   <Electricity/>  
+    <News/>
+     <Martyrs/>  
+    <Footer/>  
+    </div>
+  );
+}
+
+export default App;
